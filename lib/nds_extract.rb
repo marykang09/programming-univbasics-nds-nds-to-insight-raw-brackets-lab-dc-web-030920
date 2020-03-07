ecaeca32ca92d807ba0e7449fa41d604c6820ac0
Mary Kang <mary.kang09@gmail.com>
2020-03-07 16:19:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,9 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'directors_database'
-
 def directors_totals(nds)
   director_index = 0
   totals = {}
